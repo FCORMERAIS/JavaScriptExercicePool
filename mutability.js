dfb7bfb3d9b7c965ab0800ae2bfa1d8bab990a30
Flavio <flavio.cormerais@ynov.com>
2022-03-25 09:45:10 +0100

    first commit exo4

--- mutability.js
+++ mutability.js
@@ -1,5 +1,5 @@
-const clone1 = person
-const clone2 =person 
-const samePerson = person
+const clone1 = Object.assign({},person)
+const clone2 = clone1 
+const samePerson = clone1
 person.age = 78
 person.country = "FR"
