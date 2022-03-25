3fa26defa2e7b1bbb86158864268a050d1d7dfe4
Flavio <flavio.cormerais@ynov.com>
2022-03-25 09:34:31 +0100

    five attemp exo3

--- declarations.js
+++ declarations.js
@@ -1,36 +1,12 @@
-let str 
-let num 
-let bool 
+const escapeStr = '`\\/"\''
+const arr = [4,'2']
 let undef
-let escapeStr
-let arr 
-const obj = {
-    str: '',
-    num: 0,
-    bool:0,
-    undef:0
-  }; 
-
-
-arr = [4,'2']
-escapeStr = '`\\/"\''
-str = 'string'
-num = 1
-bool = true
-undef = undefined  
-obj.str = str,obj.bool=bool,obj.undef=undef,obj.num =num
+const obj = {str : 'string',num : 1,bool : true,undef : undefined }
 const nested = {
     arr:[4,undefined,'2'],
-    obj: {
-        str :obj.str,
-        num:obj.num,
-        bool:obj.bool
-    }
+    obj:{str:obj.str,num:obj.num,bool:obj.bool}
 }
 
-nested.obj.update = 5
-
-Object.freeze(nested.arr);
 Object.freeze(nested.obj);
 Object.freeze(nested);
 Object.freeze(obj);
