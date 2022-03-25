b99cae61c9bd8e22041b70e83550d3b093d00464
Flavio <flavio.cormerais@ynov.com>
2022-03-25 10:42:53 +0100

    second attemp exo 9

--- change.js
+++ change.js
@@ -1,5 +1,5 @@
-const get = (n) => sourceObject.n
+const get = (n) => sourceObject[n]
 function set(key, value) {
-    sourceObject.key = value
-    return sourceObject.key
+    sourceObject[n] = value
+    return value
 }
