feacac34f154ebc38a0567e1eb25d1f0cda3e6dd
Flavio <flavio.cormerais@ynov.com>
2022-03-25 19:58:36 +0100

    exo 20 first attemp

---
+++ collections.js
@@ -0,0 +1,15 @@
+const arrToSet = (n) => Set(n)
+const arrToStr = (n) => String(n)
+const setToArr = (n) => Array(n)
+const setToStr = (n) => String(n)
+const strToArr = (n) => Array(n)
+const strToSet = (n) => Set(n)
+const mapToObj = (n) => Object(n)
+const objToArr = (n) => Array(n)
+const objToMap = (n) => Map(n)
+const arrToObj = (n) => Object(n)
+const strToObj = (n) => Object(n)
+
+function superTypeOf(n) {
+    return typeof n
+}
