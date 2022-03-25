c85eabb00740b08c99dcbe5f48332d2248805c15
Flavio <flavio.cormerais@ynov.com>
2022-03-25 09:48:36 +0100

    exo4 second attemp

--- mutability.js
+++ mutability.js
@@ -1,5 +1,5 @@
-const clone1 = person
-const clone2 = person 
+const clone1 = Object.assign({},person)
+const clone2 = Object.assign({},person) 
 person.age = 78
 person.country = "FR"
 const samePerson = person
