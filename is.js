2ddf08402d8573e9665614668462aab03a8ad399
Flavio <flavio.cormerais@ynov.com>
2022-03-25 19:07:55 +0100

    exo 17 first attemp

--- is.js
+++ is.js
@@ -1,9 +1,9 @@
 is.num = (n) => typeof n == "number" ? true : false
-is.nan = (n) => typeof n == "number" ? false : true
+is.nan = (n) => Number.isNaN(n)
 is.str = (n) => typeof n == "string"? true : false
 is.bool =(n) => typeof n == "boolean" ? true : false
 is.undef = (n) => typeof n == undefined ? true : false
-is.def =(n) => typeof n == undefined ? false : true
+is.def = (n) => !is.undef(n)
 is.arr = (n) => Array.isArray(n)
 is.obj = (n) => typeof n == 'object' ? true : false
 is.fun = (n) => typeof n === "function" ? true : false
