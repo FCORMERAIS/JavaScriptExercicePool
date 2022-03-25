6dbedcf2f33b755df61be301cb8e065a5b4f04d0
Flavio <flavio.cormerais@ynov.com>
2022-03-25 12:01:42 +0100

    exo 16 first attemp

---
+++ sign.js
@@ -0,0 +1,16 @@
+function sign(n) {
+    if( n == 0){
+        return 0
+    }
+    if( n > 0) {
+        return 1
+    } else {
+        return -1
+    }
+}
+
+function sameSign(n1,n2) {
+    if (sign(n1) == sign(n2)) {
+        return true
+    }else {return false}
+}
