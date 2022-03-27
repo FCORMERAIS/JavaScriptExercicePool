c7d765998fe8059453dbab1797aadb5497a2cd34
Flavio <flavio.cormerais@ynov.com>
2022-03-27 22:12:42 +0200

    exo 23 first attemp

--- elementary.js
+++ elementary.js
@@ -1,10 +1,16 @@
 const multiply = (a,b) => {
     let count = 0
     let result = 0
-    let tempo = Math.abs(b)
-    while (tempo > count) {
+    let signe = 0 
+    if (b < 0) {
+        signe = 1
+    }
+    while (Math.abs(b) > count) {
         result = result+a
         count = count+1
+    }
+    if (signe == 1 ){
+        return -result
     }
     return result
 }
