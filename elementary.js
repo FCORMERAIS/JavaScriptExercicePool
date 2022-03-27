105650fa296ffdefc22629a53845b7ed4bc56c24
Flavio <flavio.cormerais@ynov.com>
2022-03-27 20:53:34 +0200

    exo 23 first attemp

--- elementary.js
+++ elementary.js
@@ -1,25 +1,25 @@
 const multiply = (a,b) => {
     let count = 0
     let result = 0
-    while (b !==count) {
+    while (b != count) {
         result = result+a
         count = count+1
     }
     return result
 }
 
-// const divide  = (a,b) => {
-//     let result = 0
-//     while (a >= b) {
-//         result = result +1 
-//         a = a-b
-//     }
-//     return result
-// }
+const divide  = (a,b) => {
+    let result = 0
+    while (a >= b) {
+        result = result +1 
+        a = a-b
+    }
+    return result
+}
 
-// const modulo  = (a,b) => {
-//     while (a >= b) {
-//         a = a-b
-//     }
-//     return a
-// }
+const modulo  = (a,b) => {
+    while (a >= b) {
+        a = a-b
+    }
+    return a
+}
