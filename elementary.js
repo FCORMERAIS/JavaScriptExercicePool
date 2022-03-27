020eadeb38454883744e931d9ec04df4e89562bc
Flavio <flavio.cormerais@ynov.com>
2022-03-27 20:52:05 +0200

    exo 23 first attemp

---
+++ elementary.js
@@ -0,0 +1,25 @@
+const multiply = (a,b) => {
+    let count = 0
+    let result = 0
+    while (b !==count) {
+        result = result+a
+        count = count+1
+    }
+    return result
+}
+
+const divide  = (a,b) => {
+    let result = 0
+    while (a >= b) {
+        result = result +1 
+        a = a-b
+    }
+    return result
+}
+
+const modulo  = (a,b) => {
+    while (a >= b) {
+        a = a-b
+    }
+    return a
+}
