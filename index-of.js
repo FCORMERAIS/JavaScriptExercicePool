d0cf47e3218255c7561259b343f731dbfe4ade7f
Flavio <flavio.cormerais@ynov.com>
2022-03-28 08:37:26 +0200

    exo 24 test

--- index-of.js
+++ index-of.js
@@ -16,27 +16,29 @@ const indexOf = (arr,n,begin) =>  {
 }
 
 const lastIndexOf  = (arr,n,begin) => {
-    let tempo = -1
+    let count = 0
     if (typeof begin !== 'undefined') {
-        for (let index = begin; index < arr.length; index++) {
+        for (let index = begin; count != arr.length; index--) {
             if (n === arr[index]) {
-                tempo = index
+                return index
             }
-        }
-        if (tempo !== -1) {
-            return tempo
+            count++
+            if (index == 0) {
+                index = arr.length -1 
+            }
         }
     }else {
-        for (let index = 0; index < arr.length; index++) {
+        for (let index = 0; count != arr.length; index--) {
             if (n === arr[index]) {
-                tempo =index
+                return index
+            }
+            count++
+            if (index == 0) {
+                index = arr.length -1 
             }
         }
-        if (tempo !== -1) {
-            return tempo
-        }
     }
-    return tempo
+    return -1
 }
 
 const includes  = (arr,n) => {
