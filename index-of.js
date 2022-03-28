3a2309ced14c08c7161762e74edfa77666292dc0
Flavio <flavio.cormerais@ynov.com>
2022-03-28 08:14:22 +0200

    exo 24 test

---
+++ index-of.js
@@ -0,0 +1,15 @@
+const indexOf = (arr,n) =>  {
+    for (let index = 0; index < arr.length; index++) {
+        if (n === arr[index]) {
+            return index
+        }
+    }
+}
+
+const lastIndexOf  = (arr,n) => {
+
+}
+
+const includes  = (arr,n) => {
+    
+}
