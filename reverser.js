d6d4eb02248302318f8ef514447cb66010ef0e72
Flavio <flavio.cormerais@ynov.com>
2022-03-28 11:25:31 +0200

    exo 28 test

--- reverser.js
+++ reverser.js
@@ -1,6 +1,6 @@
 const reverse = (n) => {
     let result = []
-    for (let index = n.lenght; index > 0; index--) {
+    for (let index = n.lenght-1; index > 0; index--) {
         result.push(n[index])
     }
     return result
