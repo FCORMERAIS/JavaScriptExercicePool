0ba69a157e7e58a51efe0a0fc87594f107b8f649
Flavio <flavio.cormerais@ynov.com>
2022-03-28 17:31:45 +0200

    exo 28 test

--- unbreakable.js
+++ unbreakable.js
@@ -1,21 +1,14 @@
-const split = (arr,n) => {
-    let result = []
-    let str = ""
-    for (let index = 0; index < arr.length; index++) {
-        let count = 0
-        for (let i = 0; i < n.length; i++) {
-            while (arr[index+count] === n[i+count]) {
-                count++
-            }
-        }
-        if (count == n.length) {
-            result.push(str+n)
-            str = ""
-            index =+ n.length
-        }else {
-            str += arr[index]
+const split = (str, elem) =>{
+    let arr = []
+    for (let i = 0; i < str.length - elem.length+1; i++) {
+        if (str.slice(i,i+elem.length) == elem) {
+            arr.push(str.slice(0, i))
+            str = str.slice(i + elem.length)
+            i = 0
         }
     }
+    arr.push(str)
+    return arr
 }
 
 const join = (arr,n) => {
