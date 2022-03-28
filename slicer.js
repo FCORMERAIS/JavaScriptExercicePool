e3cee453ffe37fa2b8be00071c795ab3bbf5c373
Flavio <flavio.cormerais@ynov.com>
2022-03-28 09:40:35 +0200

    exo 25 test

--- slicer.js
+++ slicer.js
@@ -11,7 +11,7 @@ const slice = (slice,n1, n2) =>{
             arr.push(slice[index])  
         }
     }else {
-        for (let index = n1; slice < n2; index++) {
+        for (let index = n1; index < n2; index++) {
             arr.push(slice[index])  
         }
     }
@@ -20,3 +20,4 @@ const slice = (slice,n1, n2) =>{
     }
     return arr
 }
+console.log(slice('abcdef', 2))
