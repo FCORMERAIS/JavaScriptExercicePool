f6f6d36363cd35778abd262bf540f5560fb81ff8
Flavio <flavio.cormerais@ynov.com>
2022-03-28 09:34:47 +0200

    exo 25 test

--- slicer.js
+++ slicer.js
@@ -1,3 +1,22 @@
 const slice = (slice,n1, n2) =>{
-
+    let type = 0
+    if (typeof slice == "string") {
+        slice = slice.split("")
+        type = 1
+    }
+    let arr = slice
+    if ( typeof n1 == "undefined") {
+    }else if (typeof n2 == "undefined") {
+        for (let index = n1; index < array.length; index++) {
+            arr.push(array[index])  
+        }
+    }else {
+        for (let index = n1; index < n2; index++) {
+            arr.push(array[index])  
+        }
+    }
+    if (type == 1) {
+        return arr.join("")
+    }
+    return arr
 }
