398049c7e49453b149ef5cd2adce75732680312f
Flavio <flavio.cormerais@ynov.com>
2022-03-29 08:48:51 +0200

    exo 30 first attemp

---
+++ molecules-cells.js
@@ -0,0 +1,29 @@
+const RNA = (arr) => {
+    for (let index = 0; index < arr.length; index++) {
+        if (arr[index] == 'G') {
+            arr[index] = arr.replace(/.$/, 'C');
+        }else if (arr[index] == 'C') {
+            arr[index] = arr.replace(/.$/, 'G');
+        }else if (arr[index] == 'T') {
+            arr[index] = arr.replace(/.$/, 'A');
+        }else  {
+            arr[index] = arr.replace(/.$/, 'U');
+        }
+    }
+    return arr
+}
+
+const DNA = (arr) => {
+    for (let index = 0; index < arr.length; index++) {
+        if (arr[index] == 'C') {
+            arr[index] = arr.replace(/.$/, 'G');
+        }else if (arr[index] == 'G') {
+            arr[index] = arr.replace(/.$/, 'C');
+        }else if (arr[index] == 'A') {
+            arr[index] = arr.replace(/.$/, 'T');
+        }else  {
+            arr[index] = arr.replace(/.$/, 'A');
+        }
+    }
+    return arr
+}
