2ec7f58794255f8a7681eb5d778ade305e60dd9f
Flavio <flavio.cormerais@ynov.com>
2022-03-29 08:14:12 +0200

    exo 28 test

--- chunky.js
+++ chunky.js
@@ -1,8 +1,10 @@
 const chunk = (arr,n) => {
     let res = []
+    let result 
     while (arr.length > n) {
         res.push(arr.slice(0,n+1))
-        arr = arr.slice(n+1,arr.length+1)
+        arr = arr.slice(n,arr.length+1)
+        result.push(res)
     }
     return res
 }
