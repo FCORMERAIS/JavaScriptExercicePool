13fc67f3b0b4a40df37ccb46a15e4619151a37de
Flavio <flavio.cormerais@ynov.com>
2022-03-29 08:31:44 +0200

    exo 29 first attemp

--- nasa.js
+++ nasa.js
@@ -1,6 +1,6 @@
 const nasa = (n) => {
     let res = ""
-    for (i=1; i <= n; i++) {
+    for (let i=1; i <= n; i++) {
         res += String(i) + ' '
         if (i%3 == 0 && i% 5 == 0) {
             res += 'NASA '
