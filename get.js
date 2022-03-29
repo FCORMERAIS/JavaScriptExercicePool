7190e617d7465d563d6fd421e2040bb4ec0c22f5
Flavio <flavio.cormerais@ynov.com>
2022-03-29 09:09:22 +0200

    exo 31 second attemp

--- get.js
+++ get.js
@@ -1,3 +1,10 @@
 const get = (src, path) => {
-    return  src.path
+    path.split('.')
+    path.forEach((element) => {
+        if (src === 'undefined') {
+            return src
+        }
+        src =src[elment]
+    });
+    return src
 }
