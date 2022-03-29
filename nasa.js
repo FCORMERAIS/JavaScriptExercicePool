ed17b9a8fbe96fb85f34f11343572a81cf584173
Flavio <flavio.cormerais@ynov.com>
2022-03-29 08:30:20 +0200

    exo 29 first attemp

---
+++ nasa.js
@@ -0,0 +1,14 @@
+count nasa = (n) => {
+    res = ""
+    for (i=1; i <= n; i++) {
+        res += String(i) + ' '
+        if (i%3 == 0 && i% 5 == 0) {
+            res += 'NASA '
+        }else if (i % 3 == 0){
+            res += 'NA '
+        }else if (i % 5 == 0) {
+            res += 'SA '
+        }
+    }
+    return res
+}
