b0249f92a1d4c2618ca60efd0b3d793985529bb6
Flavio <flavio.cormerais@ynov.com>
2022-03-29 10:02:56 +0200

    exo 31 second attemp

--- pyramid.js
+++ pyramid.js
@@ -1,7 +1,7 @@
 const pyramid = (str, n) => {
     let res = ""
     for (let i= 1;n>= i ; i++) {
-        res += ' '.repeat(n-i)+(str.repeat(i*2 -1)) + ' '.repeat(n-i)
+        res += ' '.repeat(n-i)+(str.repeat(i*2 -1))
         if (i === n) {
             return res
         }else {
