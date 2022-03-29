cd335a09d0523026625d58fd35c1afb3ea4bc6ba
Flavio <flavio.cormerais@ynov.com>
2022-03-29 09:14:56 +0200

    exo 31 second attemp

--- get.js
+++ get.js
@@ -2,7 +2,7 @@ const get = (src, path) => {
     let pathtempo = path.split('.')
     let tempo = src
     pathtempo.forEach((element) => {
-        if (tempo === 'undefined') {
+        if (tempo === undefined) {
             return src
         }
         tempo =tempo[element]
