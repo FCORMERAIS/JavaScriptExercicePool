76df2dc17e039543fb4d7fb407d528e383d62a7b
Flavio <flavio.cormerais@ynov.com>
2022-03-29 09:53:14 +0200

    exo 31 second attemp

--- triangle.js
+++ triangle.js
@@ -3,6 +3,9 @@ const triangle = (str, n) => {
     for (let i= 1;n>= i ; i++) {
         res += (i*String(str))
         res += '\n'
+        if (i === int) {
+            return res
+        }
     }   
     return res
 }
