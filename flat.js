d5c2b5d18ad5534301ed9de82eef6bcbdb48b34a
Flavio <flavio.cormerais@ynov.com>
2022-03-29 11:38:18 +0200

    exo 33 first attemp

--- flat.js
+++ flat.js
@@ -1,14 +1,18 @@
-
-// flat() method polyfill: Flatten our data down to 1 array
-Object.defineProperty(Array.prototype, 'flat',
-    {
-        value: function (depth) {
-            depth = 1;
-            return this.reduce(
-                function (flat, toFlatten) {
-                    return flat.concat((Array.isArray(toFlatten) && (depth > 1)) ? toFlatten.flat(depth - 1) : toFlatten);
-                }, []
-            );
-        },
-        configurable: true
-});
+const flat = (arr,n) =>{
+    let res = []
+    if (typeof n === "undefined") {
+        n= 1
+    }
+    for (let flatt = 0; flatt <= n; flatt++) {
+        for (let index = 0; index < arr.length; index++) {
+            if (Array.isArray(arr[index]) == false ) {
+                res.push(arr[index])
+                arr = arr.filter(function(f) { return f !== arr[index] })
+            }else {
+                arr.push(arr[index])
+                arr = arr.filter(function(f) { return f !== arr[index] })
+            }
+        }
+    }
+    return res
+}
