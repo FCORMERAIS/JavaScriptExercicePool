717c28c9e80e8c3040cc7b92043c7c3a35663860
Flavio <flavio.cormerais@ynov.com>
2022-03-30 13:43:30 +0200

    exo 45 infinite time

--- flat.js
+++ flat.js
@@ -6,7 +6,7 @@ const flat = (arr,n) =>{
     }
     while (0!== n) {
         for (let index = 0; index < arr.length; index++) {
-            console.log(arr.length,index,arr)
+            console.log(arr.length,index,arr,arr[index])
             if (Array.isArray(arr[index]) == false) {
                 res.push(arr[index])
             }else {
