2d33d8279c5a4a2844118b476337aae378c63c39
Flavio <flavio.cormerais@ynov.com>
2022-03-30 14:56:43 +0200

    exo 42 first attemp

--- flat.js
+++ flat.js
@@ -1,6 +1,7 @@
 const flat = (arr,n) =>{
     let res = []
     let tempo = []
+    let test = 0
     if (typeof n === "undefined") {
         n= 1
     }
@@ -9,23 +10,27 @@ const flat = (arr,n) =>{
     }
     while (0 !== n) {
         tempo = []
+        test = 0
         for (let index = 0; index < arr.length; index++) {
             // console.log(arr.length,index,arr,arr[index],Array.isArray(arr[index]))
             if (Array.isArray(arr[index]) == false) {
                 res.push(arr[index])
             }else {
                 for (let i = 0; i < arr[index].length; i++) {
-                    console.log(arr[index][i])
-                    if (Array.isArray(arr[index][i]) == false) {
+                        if (Array.isArray(arr[index][i]) == false) {
                         res.push(arr[index][i])
                     }else {
                         tempo.push(arr[index][i])
+                        test++
                     }
                 }
             }
         }
         arr = tempo
         n--
+        if (test> 0) {
+            return res
+        }
     }
     if (Array.isArray(arr[arr.length-1])) {
         res.push(arr[arr.length-1])
