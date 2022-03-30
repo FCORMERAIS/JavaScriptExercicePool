9f56f1794e232eb737d8042e3eaf3e4686bb703f
Flavio <flavio.cormerais@ynov.com>
2022-03-30 13:30:14 +0200

    exo 45 infinite time

--- flat.js
+++ flat.js
@@ -1,8 +1,27 @@
 const flat = (arr,n) =>{
+    let res = []
     if (typeof n === "undefined") {
         n= 1
     }
-    for (let index = 0; index < array.length; index++) {
-        let element = arr[index]
+    while (0!= n) {
+        for (let index = 0; index < arr.length; index++) {
+            console.log(Array.isArray(arr[index]) , arr[index])
+            if (Array.isArray(arr[index]) == false) {
+                res.push(arr[index])
+                arr.splice(arr.indexOf(arr[index]), 1);
+            }else {
+                for (let i = 0; i < arr.length[index]; i++) {
+                    if (Array.isArray(arr[index]) == false) {
+                        res.push(arr[index][i])
+                    }else {
+                        arr.push(arr[index][i])
+                    }
+                    arr.splice(arr.indexOf(arr[index]), 1);
+                }
+            }
+        }
+        console.log(n,arr.length)
+        n--
     }
+    return res
 }
