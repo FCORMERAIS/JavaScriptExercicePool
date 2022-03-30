5511f5cfce341c3d73ce99622b5750ce08898953
Flavio <flavio.cormerais@ynov.com>
2022-03-30 13:07:23 +0200

    exo 42 first attemp

--- flat.js
+++ flat.js
@@ -1,15 +1,13 @@
 const flat = (arr,n) =>{
     let res = []
-    let index
     if (typeof n === "undefined") {
         n= 1
     }
     while (0!== n) {
         for (let index = 0; index < arr.length; index++) {
-            index = arr.indexOf(arr[index]);
             if (Array.isArray(arr[index]) == false) {
                 res.push(arr[index])
-                arr.splice(index, 1)
+                arr.splice(index, arr.length+1)
             }else {
                 for (let i = 0; i < arr.length[index]; i++) {
                     if (Array.isArray(arr[index]) == false) {
