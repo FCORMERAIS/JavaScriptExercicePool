e55b34c6f0491cd9492e9fb1d83dac964d9d3c0d
Flavio <flavio.cormerais@ynov.com>
2022-03-30 14:52:53 +0200

    exo 42 first attemp

--- flat.js
+++ flat.js
@@ -12,6 +12,7 @@ const flat = (arr,n) =>{
                 res.push(arr[index])
             }else {
                 for (let i = 0; i < arr[index].length; i++) {
+                    console.log(arr[index][i])
                     if (Array.isArray(arr[index][i]) == false && arr[index][i] != 5) {
                         res.push(arr[index][i])
                     }else {
