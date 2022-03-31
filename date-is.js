5e3e9bcaaca5fa2e28ea47a361067f0e5d1881d1
Flavio <flavio.cormerais@ynov.com>
2022-03-31 22:14:52 +0200

    test exo dr strange

--- date-is.js
+++ date-is.js
@@ -1,7 +1,4 @@
 const isValid =(n) => {
-    if (isNaN(n.getTime()) == true) {
-        return false
-    }
     if (typeof n == 'number') {
         return true
     }else if (typeof n.getTime() === "number") {
