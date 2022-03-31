9ee589db762791767d4f8630ad388c3d57e3302e
Flavio <flavio.cormerais@ynov.com>
2022-03-31 10:23:41 +0200

    exo 45 second attemp

---
+++ group-price.js
@@ -0,0 +1,17 @@
+const groupPrice=(str)=> {
+    var re = /(USD)\d*\.\d*|[$]\d*\.\d*/g;
+    var re1 = /\d*(?=\.)/;
+    var re2 = /\d*$/;
+    let arr = str.match(re);
+    let res = [];
+    if (arr !== null) {
+      for (let elem of arr) {
+        let newarr = [];
+        newarr.push(elem);
+        newarr.push(re1.exec(elem)[0]);
+        newarr.push(re2.exec(elem)[0]);
+        res.push(newarr);
+      }
+    }
+    return res;
+  }
