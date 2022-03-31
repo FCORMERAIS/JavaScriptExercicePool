f0e6628fd4f04aea5bc89682271bb1e3fbc14aa3
Flavio <flavio.cormerais@ynov.com>
2022-03-31 10:09:27 +0200

    exo 43 first attemp

---
+++ greedy-url.js
@@ -0,0 +1,30 @@
+const getURL = (data)=> {
+    var re = /(http[s]?\:\/\/)[^\s][^\s]*(?=([\s]))/g;
+    return data.match(re);
+  }
+  const greedyQuery=(data)=> {
+    let arr = getURL(data);
+    let arr2 = [];
+    var re = /([^=]*[=]){3,}/g;
+    for (let elem of arr) {
+      if (elem.match(re) !== null) {
+        arr2.push(elem);
+      }
+    }
+    return arr2;
+  }
+  const notSoGreedy = (data) =>{
+    let arr = getURL(data);
+    let arr2 = [];
+    var re = /[=]/g;
+    for (let elem of arr) {
+      if (
+        elem.match(re) !== null &&
+        elem.match(re).length >= 2 &&
+        elem.match(re).length <= 3
+      ) {
+        arr2.push(elem);
+      }
+    }
+    return arr2;
+  }
