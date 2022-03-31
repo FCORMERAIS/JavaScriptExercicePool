ba8ee29df548f0e31a2ae2180a4604617af49008
Flavio <flavio.cormerais@ynov.com>
2022-03-31 08:17:51 +0200

    exo 43 first attemp

--- same-amount.js
+++ same-amount.js
@@ -1,7 +1,11 @@
 const sameAmount  = (n,regexe1,regexe2) => {
     const res1 = n.match(regexe1)
     const res2 = n.match(regexe2)
-    if (res1.length == res2.length) {
+    if (res1.length == null && res2 == null) {
+        return true
+    }else if (res1.length == null || res2 == null) {
+        return false
+    }else if (res1.length == res2.length) {
         return true
     }else {
         return false
