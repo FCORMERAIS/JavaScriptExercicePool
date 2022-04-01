ba8a5e16995c0fe51a02d141e47b78aaf3a17423
Flavio <flavio.cormerais@ynov.com>
2022-04-01 11:36:26 +0200

    exo 45 second attemp

--- count-leap-years.js
+++ count-leap-years.js
@@ -1,7 +1,7 @@
 const countLeapYears = (n) => {
     let a = 1
     let count = 0
-    while (n.getFullYear() != a) {
+    while (n.getFullYear()+1 != a) {
         if (new Date("02-29-"+a) === true) {
             count++
         }
