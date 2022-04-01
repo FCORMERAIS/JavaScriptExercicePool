b1af083e3e1a69825c892d9febb9b125ace399b7
Flavio <flavio.cormerais@ynov.com>
2022-04-01 15:56:42 +0200

    exo 45 second attemp

--- using-map.js
+++ using-map.js
@@ -1,7 +1,7 @@
 const citiesOnly = (n) => {
     let res = []
     for (let index = 0; index <  n.length; index++) {
-        res.push(n[index].key)
+        res.push(n[index].velue)
     }
     return n.key
 }
