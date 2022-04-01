f09144b37968b677352b76b09680a0ec87495a86
Flavio <flavio.cormerais@ynov.com>
2022-04-01 08:15:30 +0200

    exo 45 second attemp

--- get-some-time.js
+++ get-some-time.js
@@ -1,5 +1,5 @@
 const firstDayWeek = (week,year) => {
-    const res = new Date(("01-01-1000"))
+    const res = new Date("01-01-1000")
     console.log(res)
     while (res.getDay() != 0) {
         res.setDate(res.getDay()-1)
