164b7edc4f02dd7e667be8cb88fc85aa2f73086e
Flavio <flavio.cormerais@ynov.com>
2022-04-01 13:36:13 +0200

    exo 45 second attemp

--- bloody-sunday.js
+++ bloody-sunday.js
@@ -1,25 +1,18 @@
 const bloodySunday = (n) =>{
-    let newday = [
-        "Monday",
-        "Tuesday",
-        "Wednesday",
-        "Thursday",
-        "Friday",
-        "Saturday",
-      ]
-    let res =dayOfTheYear(n)
-    console.log(newday[res%6])
-    return newday[res%6]
+  let newday = ["Monday","Tuesday","Wednesday","Thursday","Friday","Saturday",]
+  let res =dayOfTheYear(n)
+  console.log(newday[res%6])
+  return newday[res%6]
 }
 
 const dayOfTheYear= (date) =>{
-    let datesince = new Date();
-    let days = 1;
+  let datesince = new Date();
+    let count = 1;
     let year = date.getFullYear();
-    datesince.setFullYear(year, 0, days);
-    while (date > datesince) {
-      days++;
+    datesince.setFullYear(year, 0, count);
+    while (count > datesince) {
+      count++;
       datesince.setDate(datesince.getDate() + 1);
     }
-    return days;
+    return count;
   }
