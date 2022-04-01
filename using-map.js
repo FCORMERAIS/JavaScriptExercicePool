3ec58b05266c297a485e7ddc7390f8f60100c0cc
Flavio <flavio.cormerais@ynov.com>
2022-04-02 01:34:31 +0200

    exo 45 second attemp

--- using-map.js
+++ using-map.js
@@ -1,7 +1,25 @@
 const citiesOnly = (n) => {
     let res = []
+    console.log(n[0])
     for (let index = 0; index <  n.length; index++) {
         res.push(n[index].key)
+    }
+    return res
+}
+
+const upperCasingStates = (n) => {
+    let tempo = []
+    let res = []
+    let tempo2 = ''
+    let res2 = []
+    for (let index = 0; index < n.length; index++) {
+        tempo = n[index].split(" ")
+        for (let i = 0; i < tempo.length; i++) {
+            tempo2 = tempo[i].split("")
+            tempo2[0].toUpperCase()
+            res2.push(tempo2.join(""))
+        }
+        res.push(res2.join(""))
     }
     return res
 }
