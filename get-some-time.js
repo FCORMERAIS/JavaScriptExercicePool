1c73547ffd911617a9ff8d488f78ca1b2608b743
Flavio <flavio.cormerais@ynov.com>
2022-04-01 08:54:43 +0200

    exo 45 second attemp

--- get-some-time.js
+++ get-some-time.js
@@ -1,7 +1,6 @@
-const firstDayWeek = (week,Y) => {
-    const res = new Date(Date.UTC(parseInt(Y, 10), 0,week*7))
+const firstDayWeek = (week,year) => {
+    const res = new Date(Date.UTC(parseInt(year, 10), 0,week*7))
     console.log(res.getDay(),)
-    let year = ""
     let month = ""
     let day = ""
     while (res.getDay() != 1) {
@@ -14,11 +13,5 @@ const firstDayWeek = (week,Y) => {
     if (res.getMonth() <= 9 ) {
         month = "0"+String(res.getMonth()+1)
     }else {month=String(res.getMonth())}
-
-    if (res.getFullYear() <= 9 ) {year = "000"+String(res.getFullYear())
-    }else if (9<res.getFullYear()<=99) {year= "00"+String(res.getMonth())
-    }else if (99<res.getFullYear() <=999){year= "0"+String(res.getFullYear)
-    }else {year = String(res.getFullYear)}
-
-    return day+'-'+month+'-'+Y
+    return day+'-'+month+'-'+year
 }
