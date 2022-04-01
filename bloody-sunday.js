637aad34418a923c90f233a2fa1096dbb0ad111a
Flavio <flavio.cormerais@ynov.com>
2022-04-01 11:59:01 +0200

    exo 45 second attemp

--- bloody-sunday.js
+++ bloody-sunday.js
@@ -10,3 +10,15 @@ const bloodySunday = (n) =>{
       res =dayOfTheYear(n)
       return newday[res%6]
 }
+
+const dayOfTheYear= (date) =>{
+    let datesince = new Date();
+    let days = 1;
+    let year = date.getFullYear();
+    datesince.setFullYear(year, 0, days);
+    while (date > datesince) {
+      days++;
+      datesince.setDate(datesince.getDate() + 1);
+    }
+    return days;
+  }
