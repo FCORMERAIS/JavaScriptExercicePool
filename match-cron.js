6d04ec38cc7e7fbcdf00e3186114b696abf9124b
Flavio <flavio.cormerais@ynov.com>
2022-04-01 11:12:27 +0200

    exo 45 second attemp

--- match-cron.js
+++ match-cron.js
@@ -1,8 +1,6 @@
 const matchCron = (cron,date) => {
-    let tempo = [date.getMinutes(),date.getHours(),date.getDate(),date.getMonth(),date.getDay()]
-    console.log(tempo)
+    let tempo = [date.getMinutes(),date.getHours(),date.getDate(),date.getMonth()+1,date.getDay()]
     cron = cron.split(" ")
-    console.log(cron)
     for (let index = 0; index < cron.length; index++) {
         if (cron[index] != '*' &&tempo[index] != cron[index]) {
             return false
