562ee1a16711d81a55b00f729e635fc0e620ae32
Flavio <flavio.cormerais@ynov.com>
2022-04-01 08:50:22 +0200

    exo 45 second attemp

--- get-some-time.js
+++ get-some-time.js
@@ -11,7 +11,7 @@ const firstDayWeek = (week,Y) => {
         day = "0"+String(res.getDate())
     }else {day = String(res.getDate())}
 
-    if (res.getMonth()+1 <= 9 ) {
+    if (res.getMonth() <= 9 ) {
         month = "0"+String(res.getMonth()+1)
     }else {month=String(res.getMonth())}
 
