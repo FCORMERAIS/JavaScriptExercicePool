ff031808ce9336885112a4df5f742f600ac19d6a
Flavio <flavio.cormerais@ynov.com>
2022-04-01 08:20:08 +0200

    exo 45 second attemp

--- get-some-time.js
+++ get-some-time.js
@@ -1,8 +1,9 @@
 const firstDayWeek = (week,year) => {
-    const res = new Date("01-01-1000")
+    const res = new Date(Date.UTC(parseInt(year, 10), 0,week*7))
     console.log(res)
+    let restext = ""
     while (res.getDay() != 0) {
         res.setDate(res.getDay()-1)
     }    
-    return res
+    return res.getFullYear()+'-'+res.getMonth()+'-'+res.getDate()
 }
