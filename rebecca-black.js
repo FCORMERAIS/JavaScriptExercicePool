5fa599f002f964e0d8629ae7bc37f4615eb1b0f8
Flavio <flavio.cormerais@ynov.com>
2022-04-01 10:41:16 +0200

    exo 45 second attemp

---
+++ rebecca-black.js
@@ -0,0 +1,15 @@
+const isFriday = (n) => {
+    if (n.getDay() == 5 ) {return true} else {return false}
+}
+
+const isWeekend = (n) => {
+    if (n.getDay() == 6 || n.getDay() == 0 ) {return true} else {return false}
+}
+
+const isLeapYear = (n) => {
+
+}
+
+const isLastDayOfMonth = (n) => {
+
+}
