a1ab7748ec289c401745dfb1974473b3545d55c7
Flavio <flavio.cormerais@ynov.com>
2022-04-02 10:51:49 +0200

    exo 45 second attemp

--- using-map.js
+++ using-map.js
@@ -8,15 +8,17 @@ const upperCasingStates = (n) => {
     let tempo = []
     let res = []
     let tempo2 = ''
-    let res2 = []
     for (let index = 0; index < n.length; index++) {
-        tempo = n[index].split(" ")
-        for (let i = 0; i < tempo.length; i++) {
-            tempo2 = tempo[i].split("")
-            tempo2[0].toUpperCase()
-            res2.push(tempo2.join(""))
-        }
-        res.push(res2)
+        tempo = capitalize(n[index])
+        res.push(tempo)
     }
     return res
+}
+
+function capitalize (n){
+    n = n.toLowerCase()
+    var tempo = n.split('') 
+    tempo[0] = tempo[0].toUpperCase()
+    n = tempo.join('')
+    return n
 }
