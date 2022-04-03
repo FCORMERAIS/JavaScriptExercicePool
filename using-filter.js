8d2cb49116a5b2d623714c1a301edb902d9b8446
Flavio <flavio.cormerais@ynov.com>
2022-04-03 13:49:29 +0200

    exo 45 second attemp

--- using-filter.js
+++ using-filter.js
@@ -1,3 +1,3 @@
 const filterShortStateName  = (n) => n.filter(word => word.length < 7)
 const filterStartVowel =(n) => n.filter((elem) => /[a|i|u|e|o|A|E|I|O|U]/gi.test(elem[0]))
-const filter5Vowels = (n) => n.filter((elem) => /[a|i|u|e|o|A|E|I|O|U]{5}/gi.test(elem))
+const filter5Vowels = (n) => n.filter((elem) => elem.match(/[a|i|u|e|o|A|E|I|O|U]/gi)>=5)
