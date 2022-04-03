7fd72b4088c880a61be40f5197653b01aa093711
Flavio <flavio.cormerais@ynov.com>
2022-04-03 22:46:56 +0200

    exo 45 second attemp

--- using-filter.js
+++ using-filter.js
@@ -6,3 +6,12 @@ let filter1DistinctVowel = (arr) =>
     let chs = (str) => str.match(new RegExp(str[0], "ig")).length == str.length;
     return chs(elem.match(/[i|a|u|e|o]/gi).join(""));
   });
+
+  let multiFilter = (arrObj) =>
+  arrObj.filter(
+    (elem) =>
+      elem["capital"].length > 7 &&
+      !/[a|i|u|e|o]/gi.test(elem["name"][0]) &&
+      /[a|i|u|e|o]/gi.test(elem["tag"]) &&
+      elem["region"] !== "South"
+  );
