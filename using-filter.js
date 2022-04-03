46ace4eb7aabfe6a75c9b74e308b6f6cc078a315
Flavio <flavio.cormerais@ynov.com>
2022-04-03 22:46:44 +0200

    exo 45 second attemp

--- using-filter.js
+++ using-filter.js
@@ -1,3 +1,8 @@
 const filterShortStateName  = (n) => n.filter(word => word.length < 7)
 const filterStartVowel =(n) => n.filter((elem) => /[a|i|u|e|o|A|E|I|O|U]/gi.test(elem[0]))
 const filter5Vowels = (n) => n.filter((elem) => elem.match(/[a|i|u|e|o|A|E|I|O|U]/gi).length>=5)
+let filter1DistinctVowel = (arr) =>
+  arr.filter(function (elem) {
+    let chs = (str) => str.match(new RegExp(str[0], "ig")).length == str.length;
+    return chs(elem.match(/[i|a|u|e|o]/gi).join(""));
+  });
