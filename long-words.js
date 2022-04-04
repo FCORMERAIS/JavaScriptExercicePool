5b69a378e06a28a14fea9c09ceacc4004b1feaed
Flavio <flavio.cormerais@ynov.com>
2022-04-04 09:53:34 +0200

    exo 60 first attem

--- long-words.js
+++ long-words.js
@@ -1,3 +1,5 @@
 const longWords = (n) => n.every(value => 5 <= value.length)
 
 const oneLongWord = (n) =>n.some(value => 10 <= value.length)
+
+const noLongWords = (n) => n.every(value => 7 !== value.length)
