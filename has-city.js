23a3c68ae3bd084ef96b442c5ce0e9069492ed94
Flavio <flavio.cormerais@ynov.com>
2022-04-04 11:38:29 +0200

    finito

--- has-city.js
+++ has-city.js
@@ -1,5 +1,5 @@
 const hasCity = (country,arr) => {
     return (city) =>{
-        return arr.include(city) ? city+ " is not a city from "+country : city + " is a city from "+country
+        return arr.includes(city) ? city+ " is not a city from "+country : city + " is a city from "+country
     }
 }
