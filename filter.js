4b3b52e9fc08c19f572b88308b0e08105e94194a
Flavio <flavio.cormerais@ynov.com>
2022-04-04 10:52:29 +0200

    exo 60 first attem

--- filter.js
+++ filter.js
@@ -9,3 +9,9 @@ const reject  = (arr,func) => {
     arr.map((cv,i,a) => func(cv,i,a) ? [] : res.push(cv))
     return res
 }
+
+const partition = (arr,func) => {
+    let res = [[],[]]
+    arr.map((cv,i,a) => func(cv,i,a) ? res[0].push(cv) : res[1].push(cv))
+    return res
+}
