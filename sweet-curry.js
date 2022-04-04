9e552f28fe5aa14bc5080cfc7694c384d22bf97b
Flavio <flavio.cormerais@ynov.com>
2022-04-04 11:09:03 +0200

    exo 60 first attem

--- sweet-curry.js
+++ sweet-curry.js
@@ -1,3 +1,24 @@
-const mult2 = curry((x, y) => x * y);
-const add3 = curry((x, y, z) => x + y + z);
-const sub4 = curry((x, y, z, w) => x - y - z - w);
+
+const mult2 = (a) => {
+    return (b) => {
+        return a * b;
+    }
+}
+
+const add3 = (a) => {
+    return (b) => {
+        return (c) => {
+            return a + b + c;
+        }
+    }
+}
+
+const sub4 = (a) => {
+    return (b) => {
+        return (c) => {
+            return (d) => {
+                return a - b - c - d;
+            }
+        }
+    }
+}
