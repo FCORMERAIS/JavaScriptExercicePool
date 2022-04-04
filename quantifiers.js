22500fabc017a29eb6212f0c4a619d0bc9749973
Flavio <flavio.cormerais@ynov.com>
2022-04-04 10:58:33 +0200

    exo 60 first attem

--- quantifiers.js
+++ quantifiers.js
@@ -1,1 +1,5 @@
 const every = (arr, fn) => arr.reduce((acc, val) => acc && fn(val), true)
+
+const some = (arr, fn) => arr.reduce((acc, val) => acc || fn(val), false)
+
+const none = (arr, fn) => arr.reduce((acc, val) => acc && !fn(val), true)
