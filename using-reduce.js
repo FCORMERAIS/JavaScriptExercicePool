cdd76ef43ff852d1b3f4dda7c334b7aa8d3ad167
Flavio <flavio.cormerais@ynov.com>
2022-04-04 08:40:45 +0200

    exo 60 first attem

--- using-reduce.js
+++ using-reduce.js
@@ -1,4 +1,5 @@
 const adder = (n) => {  
+    const initialValue = 0
     const sumWithInitial = n.reduce((previousValue, currentValue) => previousValue + currentValue,initialValue)
     return sumWithInitial
 }
