37c1d910852bfa27c0b6cadb8f4fb06ae3b4ae5d
Flavio <flavio.cormerais@ynov.com>
2022-04-04 16:39:12 +0200

    hello

--- flow.js
+++ flow.js
@@ -2,7 +2,7 @@ const flow = (arr) => (...args) => {
     let temp = arr[0](...args)
     arr = arr.slice(1)
     for (let each of arr) {
-        temp = each.apply(x, [temp])
+        temp = each.apply(this, [temp])
     }
     return temp
 }
