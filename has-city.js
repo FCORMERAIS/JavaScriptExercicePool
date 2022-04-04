b687e4a1d3b7fd7ce0dbb7316bc9d8a4e035a797
Flavio <flavio.cormerais@ynov.com>
2022-04-04 11:17:19 +0200

    exo 60 first attem

--- has-city.js
+++ has-city.js
@@ -1,5 +1,5 @@
 const hasCity = (country,arr) => {
     return (city) =>{
-        return city === country.city
+        return city === country.city ? city+ "is a city from "+country.name : city + "is not a city from "+country.name
     }
 }
