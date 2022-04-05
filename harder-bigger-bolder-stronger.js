abd1b246d235bd86e1a1815aa90ad4947eed3b22
Flavio <flavio.cormerais@ynov.com>
2022-04-05 23:50:38 +0200

    test

--- harder-bigger-bolder-stronger.js
+++ harder-bigger-bolder-stronger.js
@@ -2,18 +2,17 @@ export function generateLetters() {
     let app = document.querySelector("body");
     let random = 0
     const letter = "abcdefghijklmnopqrstuvwxyz".toUpperCase().split("");
-    console.log(letter);
     for (let index = 0; index < 120; index++) {
         const newDiv = document.createElement("div");
         random = Math.round(Math.random() * 25)
-        newDiv.textContent(letter[random]);
+        newDiv.textContent = letter[random];
         if (index < 40) {
-            newDiv.style.cssText = "font-size: " + (11 + ind) + "px; font-weight: " + 300 + ";";
+            newDiv.style.cssText = "font-size: " + (11+index) + "px; font-weight: " + 300 + ";";
         } else if (index < 80) {
-            newDiv.style.cssText = "font-size: " + (11 + ind) + "px; font-weight: " + 400 + ";";
+            newDiv.style.cssText = "font-size: " + (11+index) + "px; font-weight: " + 400 + ";";
         } else {
-            newDiv.style.cssText = "font-size: " + (11 + ind) + "px; font-weight: " + 600 + ";";
+            newDiv.style.cssText = "font-size: " + (11+index) + "px; font-weight: " + 600 + ";";
         }
-        app.append(nextDiv);
+        app.append(newDiv);
     }
 }
