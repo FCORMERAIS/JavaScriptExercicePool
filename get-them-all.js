8c80d9c6beb465712b1d0ea440348bbc149d5273
Flavio <flavio.cormerais@ynov.com>
2022-04-05 08:55:38 +0200

    get-them-all

--- get-them-all.js
+++ get-them-all.js
@@ -1,14 +1,24 @@
-const getArchitects =(n)=>{
+export function getArchitects() {
+    var elements = document.getElementsByTagName("a")
+    var elements2 = document.getElementsByTagName("span")
+    let arr1 = []
+    let arr2 = []
+    for (let architect of elements) {
+        arr1.push(architect)
+    }
+    for (let architect of elements2) {
+        arr2.push(architect)
+    }
+    return [arr1,arr2]
+}
+
+export function getClassical() {
+}
+
+export function getActive() {
 
 }
 
-const getClassical = (n)=>{
-}
+export function getBonannoPisano() {
 
-const getActive  = (n) => {
-
-}
-
-const getBonannoPisano = (n) => {
-    
 }
