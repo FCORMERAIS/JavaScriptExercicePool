69965c35917126555122b60964aaaedbb514c19d
Flavio <flavio.cormerais@ynov.com>
2022-04-05 09:24:32 +0200

    get-them-all

--- get-them-all.js
+++ get-them-all.js
@@ -13,8 +13,8 @@ export function getArchitects() {
 }
 
 export function getClassical() {
-    var element = document.getElementsByClassName('classical')
-    var element2 = document.getElementsByClassName(!'classical')
+    var element = document.getElementsByClassName('a.classical')
+    var element2 = document.getElementsByClassName("a:not(.classical)")
     for (let architect of elements) {
         arr1.push(architect)
     }
@@ -25,9 +25,22 @@ export function getClassical() {
 }
 
 export function getActive() {
-
+    var element = document.getElementsByClassName('a.classical.active')
+    var elemnt2 = document.getElementsByClassName("a.classical:not(.active)")
+    for (let architect of elements) {
+        arr1.push(architect)
+    }
+    for (let architect of elements2) {
+        arr2.push(architect)
+    }
+    return [arr1,arr2]
 }
 
 export function getBonannoPisano() {
-
+    var container = document.querySelector("#BonannoPisano");
+    var elements2 = container.querySelectorAll("a:not(#BonannoPisano).classical.active");
+    for (let architect of elements2) {
+        arr2.push(architect)
+    }
+    return [container,arr2]
 }
