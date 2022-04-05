dce91eb80433c3242c9dd70e1810e3265433377b
Flavio <flavio.cormerais@ynov.com>
2022-04-05 09:27:30 +0200

    get-them-all

--- get-them-all.js
+++ get-them-all.js
@@ -15,6 +15,8 @@ export function getArchitects() {
 export function getClassical() {
     var element = document.getElementsByClassName('a.classical')
     var element2 = document.getElementsByClassName("a:not(.classical)")
+    let arr1 = []
+    let arr2 = []
     for (let architect of elements) {
         arr1.push(architect)
     }
@@ -27,6 +29,8 @@ export function getClassical() {
 export function getActive() {
     var element = document.getElementsByClassName('a.classical.active')
     var elemnt2 = document.getElementsByClassName("a.classical:not(.active)")
+    let arr1 = []
+    let arr2 = []
     for (let architect of elements) {
         arr1.push(architect)
     }
@@ -39,8 +43,14 @@ export function getActive() {
 export function getBonannoPisano() {
     var container = document.querySelector("#BonannoPisano");
     var elements2 = container.querySelectorAll("a:not(#BonannoPisano).classical.active");
+    let arr1 = []
+    let arr2 = []
+    for (let architect of elements) {
+        arr1.push(architect)
+    }
     for (let architect of elements2) {
         arr2.push(architect)
     }
-    return [container,arr2]
+    return [arr1[0],arr2]
+
 }
