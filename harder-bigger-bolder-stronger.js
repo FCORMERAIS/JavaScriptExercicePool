b75ddf188e05496a09f0ead53cc57f43560d484e
Flavio <flavio.cormerais@ynov.com>
2022-04-05 23:45:38 +0200

    test

--- harder-bigger-bolder-stronger.js
+++ harder-bigger-bolder-stronger.js
@@ -1,7 +1,7 @@
 export function generateLetters() {
     let app = document.querySelector("body");
     let random = 0
-    const letter = "abcdefghijklmnopqrstuvwxyz".toUpperCase().split("");
+    const letter = "abcdefghijklmnopqrstuvwxyz".toUpperCase();
     console.log(letter);
     for (let index = 0; index < 120; index++) {
         const newDiv = document.createElement("div");
