608c473bb9daea67fefa70f50b55a070007b6c2c
Flavio <flavio.cormerais@ynov.com>
2022-04-06 17:42:15 +0200

    testé é

--- pimp-my-style.js
+++ pimp-my-style.js
@@ -1,36 +1,30 @@
-import { styles } from "./data.js";
-export const pimp = () => {
-  let butt = document.querySelector("button");
-  let clicks = 0;
-  if (butt.hasAttribute("data-clicks")) {
-    clicks = parseInt(butt.dataset.clicks);
-    console.log(clicks, butt.dataset.clicks);
-  }
-  butt.dataset.clicks = clicks;
-  if (butt.classList.contains("unpimp")) {
-    if (butt.hasAttribute("data-clicks")) {
-      clicks = parseInt(butt.dataset.clicks);
-      console.log(clicks, butt.dataset.clicks);
-    }
-    butt.classList.remove(styles[clicks]);
-    clicks--;
-    if (clicks == -1) {
-      butt.classList.remove("unpimp");
-      clicks++;
-    }
-  } else {
-    if (butt.hasAttribute("data-clicks")) {
-      clicks = parseInt(butt.dataset.clicks);
-      console.log(clicks, butt.dataset.clicks);
-    }
-
-    butt.classList.add(styles[clicks]);
-    clicks++;
-    if (clicks == 15) {
-      butt.classList.add("unpimp");
-      clicks--;
-    }
-  }
-  butt.dataset.clicks = clicks;
-  console.log("set click", clicks);
+import {styles} from './data.js'
+let i = 0
+let j = 14
+let k = 0
+export function pimp(){
+    let elem = document.querySelector("button")
+    let mat = Math.floor(i/styles.length)
+    console.log(elem.classList)
+    console.log(j)
+        if (mat%2 == 0) {
+            j = 14
+            elem.classList.add(styles[k])
+            i++
+            k++
+            if(k == 15){
+                elem.classList.add('unpimp')
+            }
+        }
+        if (mat%2 != 0){
+            k = 0
+            elem.classList.add('unpimp')
+            elem.classList.remove(styles[j])
+            i++
+            j--
+            if(j == -1){
+                elem.classList.remove('unpimp')
+            }
+            
+     }
 }
