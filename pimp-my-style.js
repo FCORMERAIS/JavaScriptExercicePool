b21dc6934025f556446f4abd3b46cac353838440
Flavio <flavio.cormerais@ynov.com>
2022-04-06 22:26:35 +0200

    site pimp

--- pimp-my-style.js
+++ pimp-my-style.js
@@ -1,30 +1,21 @@
 import {styles} from './pimp-my-style.data.js'
-let i = 0
-let j = 14
-let k = 0
-export function pimp(){
-    let elem = document.querySelector("button")
-    let mat = Math.floor(i/styles.length)
-    console.log(elem.classList)
-    console.log(j)
-        if (mat%2 == 0) {
-            j = 14
-            elem.classList.add(styles[k])
-            i++
-            k++
-            if(k == 15){
-                elem.classList.add('unpimp')
-            }
+let count = 0
+let addRemove = 0
+export function pimp() {
+    let element = document.querySelector("button");
+    if (count < styles.length && addRemove == 0) {
+        element.classList.add(styles[count]);
+        count++
+        if (count == 15) {
+            addRemove = 1
+            element.classList.add("unpimp");
         }
-        if (mat%2 != 0){
-            k = 0
-            elem.classList.add('unpimp')
-            elem.classList.remove(styles[j])
-            i++
-            j--
-            if(j == -1){
-                elem.classList.remove('unpimp')
-            }
-            
-     }
+    }else {
+        count--
+        element.classList.remove(styles[count])
+        if (count == 0) {
+            addRemove = 0
+            element.classList.remove("unpimp");
+        }
+    }
 }
