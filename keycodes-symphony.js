51e173a578725d2d4acb53367ec2a6708b37f51c
Flavio <flavio.cormerais@ynov.com>
2022-04-07 08:58:07 +0200

    dodo

--- keycodes-symphony.js
+++ keycodes-symphony.js
@@ -4,8 +4,9 @@ export function compose(){
         if (keypressed == "Backspace"){
             document.querySelectorAll("div")[document.querySelectorAll("div").length-1].remove()
         }else if (keypressed == "Escape"){
-            let removeLastChar = document.querySelectorAll("div")
-            removeLastChar[removeLastChar.length-1].remove()
+            document.querySelectorAll("div").forEach((element) => {
+                element.remove()
+            })
         }else {
             let elem = document.createElement('div')
             elem.textContent = keypressed
