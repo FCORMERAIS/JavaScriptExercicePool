4a039235b8d3bf5be5cff13a49689b9a80702f5c
Flavio <flavio.cormerais@ynov.com>
2022-04-07 11:12:52 +0200

    test

---
+++ where-do-we-go.js
@@ -0,0 +1,15 @@
+import { places } from './data.js';
+export function explore() {
+    for (let i = 0; i < places.length; i++) {
+
+        let section = document.createElement("section")
+        section.style.backgroundImage = "url(./images/" + places[i].name.split(',')[0].toLowerCase().split(' ').join('-') + ".jpg)"
+        section.style.backgroundSize = "100%"
+        section.className = "location"
+        let link = document.createElement('a')
+        link.innerHTML = places[i].name + places[i].coordinates
+        link.style.color = places[i].color
+        section.appendChild(link)
+        document.body.appendChild(section)
+    }
+}
