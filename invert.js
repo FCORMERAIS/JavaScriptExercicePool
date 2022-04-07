158eac80bf1b10674cb9f8f69d7832603d471ecc
Flavio <flavio.cormerais@ynov.com>
2022-04-07 15:41:33 +0200

    test

--- invert.js
+++ invert.js
@@ -1,9 +1,1 @@
-const invert = (obj) => {
-    let res = {}
-    for (let k in obj) {
-        if (obj.hasOwnProperty(k)) {
-            res[obj[k]] = k
-        }
-    }
-    return res
-}
+const invert = obj => Object.fromEntries(Object.entries(obj).map(a => a.reverse()))
