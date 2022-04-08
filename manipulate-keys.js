d584e23271cbcd3c6560544832edd5344f41e09c
Flavio <flavio.cormerais@ynov.com>
2022-04-08 11:00:31 +0200

    test

--- manipulate-keys.js
+++ manipulate-keys.js
@@ -1,4 +1,4 @@
-function filterValues(obj,func) {
+function filterKeys(obj,func) {
     let res = {}
     for(let [x,y] of Object.keys(obj)) {
         if (func(y)) {
@@ -8,13 +8,13 @@ function filterValues(obj,func) {
     return res
 }
 
-function mapValues (obj,func) {
+function mapKeys(obj,func) {
     let res = {}
     for(let [x,y] of Object.keys(obj))
         res[x] = func(y)
     return res
 }
 
-function reduceValues (obj,func,initialValue) {
+function reduceKeys(obj,func,initialValue) {
     return initialValue == undefined ? Object.keys(obj).reduce(func) : Object.keys(obj).reduce(func,initialValue)
 }
