8b319ef830d57c9e2bb235c8bb5ba092ef2db2da
Flavio <flavio.cormerais@ynov.com>
2022-04-08 10:51:12 +0200

    test

---
+++ manipulate-values.js
@@ -0,0 +1,19 @@
+function filterValues(obj,func) {
+    let res = {}
+    for(let [x,y] of Object.entries(obj)) {
+        if (func(y)) {
+            res[x] = y
+        }
+    }
+}
+
+function mapValues (obj,func) {
+    let res = {}
+    for(let [x,y] of Object.entries(obj))
+        res[x] = func(y)
+    return res
+}
+
+function reduceValues (obj,func,initialValue) {
+    return initialValue == undefined ? Object.values(obj).reduce(func) : Object.values(obj).reduce(func,initialValue)
+}
