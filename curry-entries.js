15d0c84657a9d04a37ef07ed9ed05658d76f5545
Flavio <flavio.cormerais@ynov.com>
2022-04-11 11:05:16 +0200

    hello

--- curry-entries.js
+++ curry-entries.js
@@ -1,9 +1,30 @@
-function defaultCurry (obj,replace) {
-    let res = Object.assign({},obj)
-    Object.keys(replace).forEach((k)=>res[k]=replace[k])
-    return res
+function defaultCurry (obj) {
+    return (replace) => {
+        let res = Object.assign({},obj)
+        Object.keys(replace).forEach((k)=>res[k]=replace[k])
+        return res
 }
 
 function mapCurry (func,obj) {
+    return (obj) => Object.fromEntries(Object.entries(obj).map(func))
+}
 
+function reduceCurry (func,obj,initialValue) {
+    return (obj) => Object.values(obj).reduce(func,initialValue)
+}
+
+function filterCurry (func,obj) {
+    return (obj) => Object.fromEntries(Object.entries(obj).filter(func))
+}
+
+function reduceScore (obj,initialValue) {
+    return (obj) => Object.values(obj).reduce((a,b) => a+b,initialValue)
+}
+
+function filterForce (obj) {
+    return (obj) => Object.fromEntries(Object.entries(obj) > 80 ? obj : {})
+}
+
+function mapAverage(obj) {
+    return (obj) => Object.fromEntries(Object.entries(obj).map(([k,v]) => [k,v/Object.keys(obj).length]))
 }
