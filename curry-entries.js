3743d13fdd27a16c9bb590a978cca2d419045455
Flavio <flavio.cormerais@ynov.com>
2022-04-11 11:07:15 +0200

    hello

--- curry-entries.js
+++ curry-entries.js
@@ -1,31 +1,30 @@
-function defaultCurry (obj) {
-    return (replace) => {
-        let res = Object.assign({},obj)
-        Object.keys(replace).forEach((k)=>res[k]=replace[k])
+const defaultCurry = (obj1) => {
+    return (obj2) => {
+        let res = Object.assign({},obj1)
+        Object.keys(obj2).forEach((k)=>res[k]=obj2[k])
         return res
     }
 }
 
-function mapCurry (func) {
+const mapCurry = (func) => {
     return (obj) => Object.fromEntries(Object.entries(obj).map(func))
 }
 
-function reduceCurry (func,initialValue) {
-    return (obj) => Object.values(obj).reduce(func,initialValue)
+const reduceCurry = (func) => {
+    return (obj,iv) => Object.entries(obj).reduce(func, iv)
 }
 
-function filterCurry (func,obj) {
-    return (obj) => Object.fromEntries(Object.entries(obj).filter(func))
+const filterCurry = (func) => {
+    return (obj) =>  Object.fromEntries(Object.entries(obj).filter(func))
 }
 
-function reduceScore (obj,initialValue) {
-    return (obj) => Object.values(obj).reduce((a,b) => a+b,initialValue)
-}
+const forceUsers = filterCurry(([k,v]) => v.isForceUser)
 
-function filterForce (obj) {
-    return (obj) => Object.fromEntries(Object.entries(obj) > 80 ? obj : {})
-}
+const reduceScore = (obj,iv) => reduceCurry((acc,[k,v]) => acc + v.shootingScore+ v.pilotingScore)(forceUsers(obj),iv)
 
-function mapAverage(obj) {
-    return (obj) => Object.fromEntries(Object.entries(obj).map(([k,v]) => [k,v/Object.keys(obj).length]))
-}
+const filterForce = (obj) => filterCurry(([k,v])=>v.shootingScore >= 80)(forceUsers(obj))
+
+const mapAverage = (obj) => mapCurry(([k,v])=>{
+    let nv = {...v,averageScore: (v.shootingScore+v.pilotingScore)/2}
+    return [k,nv]
+})(obj)
