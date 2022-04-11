3eb0244e432d7de1fde55351967e9cc1a072a9f8
Flavio <flavio.cormerais@ynov.com>
2022-04-11 11:40:11 +0200

    hello

--- deep-copy.js
+++ deep-copy.js
@@ -3,6 +3,8 @@ const superTypeOf = (arg) => toString.call(arg).slice(8, -1)
 function deepCopy (thing) {
     let res
     if (superTypeOf(thing) == "Object") {
+        res = {}
+        Object.keys(thing).forEach((k)=>res[k]=deepCopy(thing[k]))
     }
     else if (superTypeOf(thing) == "Array") {
         res = []
