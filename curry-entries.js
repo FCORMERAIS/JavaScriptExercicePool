334769a180814e9329e732861762b4ba6e430fbd
Flavio <flavio.cormerais@ynov.com>
2022-04-11 11:05:38 +0200

    hello

--- curry-entries.js
+++ curry-entries.js
@@ -3,6 +3,7 @@ function defaultCurry (obj) {
         let res = Object.assign({},obj)
         Object.keys(replace).forEach((k)=>res[k]=replace[k])
         return res
+    }
 }
 
 function mapCurry (func,obj) {
